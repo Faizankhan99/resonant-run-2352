f453ca2857ea278d7bad75bec394a94019819d8c
Faizan Khan <103636989+Faizankhan99@users.noreply.github.com>
2022-07-23 19:26:20 +0530

    Delete pricing.js

--- pricing.js
+++
@@ -1,9 +0,0 @@
-const q=document.querySelectorAll('.question');
-const a=document.querySelectorAll('.answer');
-const arr=document.querySelectorAll('.arrow');
-for(let i=0;i<q.length;i++){
-    q[i].addEventListener('click',() => {
-a[i].classList.toggle('answer-opened');
-arr[i].classList.toggle('arrow-rotated');
-    });
-}
